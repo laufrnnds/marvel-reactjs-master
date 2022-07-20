09f465f8ba16723947eb3a52a14b6051e96f6482
Laura Fernandes <lauraifernandes2@gmail.com>
2022-07-20 16:37:41 -0300

    teste2

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 
 import { marvel } from '../../services';
 
@@ -9,8 +12,11 @@ import DynamicAccordion, {
 } from '../../components/DynamicAccordion/DynamicAccordion';
 import { ICharacters } from '../../store/modules/characters/reducer';
 import {
+  Comics,
   clearComic,
   createComic,
+  requestComic,
+  requestComicError,
 } from '../../store/modules/comics/comicsSlice';
 
 const Characters: React.FC = () => {
@@ -46,18 +52,72 @@ const Characters: React.FC = () => {
     }
   }, [charactersRedux]);
 
+  useEffect(() => {
+    dispatch(requestComic());
+    marvel
+      .get('/comics')
+      .then(({ data }) => {
+        const res = JSON.parse(data);
+        console.log(res);
+        const id = res.data.results.map((dado: any) => dado.id);
+        const title = res.data.results.map((dado: any) => dado.title);
+        const foto = res.data.results.map((dado: any) => dado.thumbnail.path);
+
+        const comics: Comics[] = [];
+        id.forEach((element: any, index: any) => {
+          comics.push({
+            id: element,
+            name: title[index],
+            imgPath: foto[index],
+          });
+        });
+
+        dispatch(createComic(comics));
+      })
+      .catch((error) => {
+        dispatch(requestComicError(error.message));
+      });
+  }, []);
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h3" color="primary">
-          Personagens - {comicRedux}
+          Personagens
         </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <DynamicAccordion items={characters} />
+        {comicRedux.loading && <div>Loading...</div>}
+        {!comicRedux.loading && comicRedux.error ? (
+          <div>Erro: {comicRedux.error}</div>
+        ) : null}
+
+        {comicRedux.comics.length > 0 && (
+          <Box>
+            {comicRedux.comics.map((dado: any) => (
+              <div key={dado.id}>
+                <Typography variant="h3" color="primary">
+                  {dado.name}
+                </Typography>
+                <img src={`${dado.imgPath}.jpg`} alt="marvel-img" />
+              </div>
+            ))}
+          </Box>
+        )}
       </Grid>
     </Grid>
   );
+
+  // return (
+  //   <Grid container spacing={2}>
+  //     <Grid item xs={12}>
+  //       <Typography variant="h3" color="primary">
+  //         Personagens - {comicRedux}
+  //       </Typography>
+  //     </Grid>
+  //     <Grid item xs={12}>
+  //       <DynamicAccordion items={characters} />
+  //     </Grid>
+  //   </Grid>
+  // );
 };
 
 export default Characters;
--- src/store/modules/comics/comicsSlice.ts
+++ src/store/modules/comics/comicsSlice.ts
@@ -1,13 +1,55 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = '';
+// const initialState = '';
+
+// const comicSlice = createSlice({
+//   name: 'comic',
+//   initialState,
+//   reducers: {
+//     createComic(state, action) {
+//       return action.payload;
+//     },
+//     clearComic() {
+//       return initialState;
+//     },
+//   },
+// });
+
+export type Comics = {
+  id: string;
+  name: string;
+  imgPath: string;
+};
+
+type initialState = {
+  loading: boolean;
+  comics: Comics[];
+  error: '';
+};
+
+const initialState: initialState = {
+  loading: false,
+  comics: [],
+  error: '',
+};
 
 const comicSlice = createSlice({
   name: 'comic',
   initialState,
   reducers: {
+    requestComic(state) {
+      state.loading = true;
+    },
     createComic(state, action) {
-      return action.payload;
+      state.loading = false;
+      state.comics = action.payload;
+      state.error = '';
+    },
+    requestComicError(state, action) {
+      state.loading = false;
+      state.comics = [];
+      state.error = action.payload;
     },
     clearComic() {
       return initialState;
@@ -15,5 +57,7 @@ const comicSlice = createSlice({
   },
 });
 
-export const { createComic, clearComic } = comicSlice.actions;
+// export const { createComic, clearComic } = comicSlice.actions;
+export const { createComic, clearComic, requestComic, requestComicError } =
+  comicSlice.actions;
 export default comicSlice.reducer;
