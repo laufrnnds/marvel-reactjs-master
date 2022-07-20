64e8d72139092e0bed639036486ce2f22eec9bd3
Laura Fernandes <lauraifernandes2@gmail.com>
2022-07-20 16:04:07 -0300

    okokokok

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -1,67 +1,30 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-console */
-import { Box, Grid, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { ItemAccordionI } from '../../components/DynamicAccordion/DynamicAccordion';
+import { Grid, Typography } from '@mui/material';
 
 import { marvel } from '../../services';
-import { ICharacters } from '../../store/modules/characters/reducer';
-import {
-  Comics,
-  createComic,
-  requestComic,
-  requestComicError,
-} from '../../store/modules/comics/comicsSlice';
+
+import DynamicAccordion, {
+  ItemAccordionI,
+} from '../../components/DynamicAccordion/DynamicAccordion';
+import { clearComic } from '../../store/modules/comics/comicsSlice';
 
 const Characters: React.FC = () => {
   const dispatch = useDispatch();
   const [characters, setCharacters] = useState<ItemAccordionI[]>([]);
 
-  const charactersRedux: ICharacters = useSelector(
-    (state: any) => state.characters
-  );
-
   const comicRedux = useSelector((state: any) => state.comic);
-
-  async function run() {
-    const response = await marvel.get('/characters');
-    console.log(response);
-  }
+  const charactersRedux = useSelector((state: any) => state.characters);
 
   async function getCharacters() {
     const result = await marvel.get('/characters');
-    console.log(`result${result}`);
+    console.log(result);
   }
 
   useEffect(() => {
+    dispatch(clearComic());
     getCharacters();
-    dispatch(requestComic());
-    marvel
-      .get('/comics')
-      .then(({ data }) => {
-        const res = JSON.parse(data);
-        const id = res.data.results.map((dado: any) => dado.id);
-        const title = res.data.results.map((dado: any) => dado.title);
-        const foto = res.data.results.map((dado: any) => dado.thumbnail.path);
-
-        const comics: Comics[] = [];
-        id.forEach((element: any, index: any) => {
-          comics.push({
-            id: element,
-            name: title[index],
-            imgPath: foto[index],
-          });
-        });
-
-        dispatch(createComic(comics));
-      })
-      .catch((error) => {
-        dispatch(requestComicError(error.message));
-      });
   }, []);
-
-  run();
 
   useEffect(() => {
     if (charactersRedux.result.length) {
@@ -83,24 +46,11 @@ const Characters: React.FC = () => {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h3" color="primary">
-          Personagens
+          Personagens - {comicRedux}
         </Typography>
-        {comicRedux.loading && <div>Loading...</div>}
-        {!comicRedux.loading && comicRedux.error ? (
-          <div>Erro: {comicRedux.error} </div>
-        ) : null}
-        {comicRedux.comics.length > 0 && (
-          <Box>
-            {comicRedux.comics.map((dado: any) => (
-              <div key={dado.id}>
-                <Typography variant="h4" color="primary">
-                  {dado.name}
-                </Typography>
-                <img src={`${dado.imgPath}.jpg`} alt="" />
-              </div>
-            ))}
-          </Box>
-        )}
+      </Grid>
+      <Grid item xs={12}>
+        <DynamicAccordion items={characters} />
       </Grid>
     </Grid>
   );
